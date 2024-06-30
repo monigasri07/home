27870466502977598e5e388f5ece93c6b02f7826
monigasri07 <monigasri.cs23@bitsathy.ac.in>
2024-06-30 22:08:16 +0530

    quotes.js

--- quotes.js
+++ quotes.js
@@ -1,17 +1,1 @@
-const quotes = [
-    "The journey of a thousand miles begins with one step.",
-    "Take care of your body. It's the only place you have to live.",
-    "Health is not just about what you're eating. It's also about what you're thinking and saying.",
-    "You are stronger than you think.",
-    "Believe you can and you're halfway there."
-];
-
-function displayDailyQuote() {
-    const quoteElement = document.getElementById('quote');
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    quoteElement.textContent = quotes[randomIndex];
-}
-
-// Call displayDailyQuote once when the page loads
-window.onload = displayDailyQuote;
 
